0b24d3da64805cab88bdd90e016c2b282942174a
UkioMaeni <99916465+UkioMaeni@users.noreply.github.com>
2022-07-03 23:24:00 +0500

    clienttable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { create, deleted, read, update } from "./http/httpReq";
+import "./App.css";
+import Header from "./component/Header/Header";
+import Body from "./component/Body/Body";
+import { useEffect, useState } from "react";
+import { cellTableSlice } from "./store/reducers/cellTableReducer";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const { setList, editPage, editSort } = cellTableSlice.actions;
+  const { list, page, count, cellFilter, typeFilter, valueFilter, sort } =
+    useSelector((state) => state.cellTableReducer);
+  const dispatch = useDispatch();
+
+  //////////////////////////обновление данных по изменению useState( реагирует на изменение, как следствие отправляет данные на сервер)
+  const [edit, setEdit] = useState(false);
+  useEffect(() => {
+    read(cellFilter, typeFilter, valueFilter, page, sort).then((e) =>
+      dispatch(
+        setList({
+          list: e.data.list,
+          count: e.data.count,
+          filter: e.data.filter,
+        })
+      )
+    );
+  }, [edit]);
+
+  ////////////////////функции управления страницей и сортировки данных////////
+  function setPage(page) {
+    dispatch(editPage(page));
+  }
+  function sortList(param) {
+    dispatch(editSort(param));
+  }
+  ////////////////////функции CRUD запросов////////
+  function createList() {
+    create().then((e) => dispatch(setList(e.data)));
+  }
+
+  function deleteList(param) {
+    deleted(param).then((e) => {
+      if (e.data === "ok") {
+        setEdit(!edit);
+      }
+    });
+  }
+
+  function updateList(id, timeT, nameT, countT, distanceT) {
+    update({ id, timeT, nameT, countT, distanceT });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header setEdit={setEdit} edit={edit} />
+      <Body
+        list={list}
+        createList={createList}
+        deleteList={deleteList}
+        page={page}
+        count={count}
+        edit={edit}
+        setEdit={setEdit}
+        setPage={setPage}
+        sort={sort}
+        sortList={sortList}
+        updateList={updateList}
+      />
     </div>
   );
 }
